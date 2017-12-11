ad28c9cf1f3d61807266da48e53501eac03d21a5
Ahmed <ghoul.ahmed5@gmail.com>
2017-12-11 22:56:10 +0100

    create user

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,27 @@
 var express = require("express");
 var router = express.Router();
+var uuid =  require('uuid/v4');
 
 var users = [
-  {id:1, name: "ahmed", age: 28, image: "" },
-  {id:2, name: "ismail", age: 35, image: "" },
-  {id:3, name: "aymen", age: 30, image: "" }
+  { id: 1, name: "ahmed", age: 28, image: "" },
+  { id: 2, name: "ismail", age: 35, image: "" },
+  { id: 3, name: "aymen", age: 30, image: "" }
 ];
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
-  res.send({users});
+  res.send({ users });
 });
 /* GET users listing. */
 router.get("/:id", function(req, res, next) {
-  res.send(users.find(res=> res.id == req.params.id));
+  res.send(users.find(res => res.id == req.params.id));
+});
+
+router.post("/create", function(req, res, next) {
+  user = req.body;
+  user.id = uuid();
+  users.push(user);
+  res.send(user);
 });
 
 module.exports = router;
